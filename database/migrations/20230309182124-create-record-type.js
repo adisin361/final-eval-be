4a70f27c518dbbd08883b4e029afd10c9827b278
Aditya Sinha <adityasinha361@gmail.com>
2023-03-10 02:19:13 +0530

    feat: add migrations

---
+++ database/migrations/20230309182124-create-record-type.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-vars */
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('RecordTypes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      fields: {
+        type: Sequelize.JSON,
+        defaultValue: {}
+      },
+      collection_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Collections',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('RecordTypes');
+  }
+};
