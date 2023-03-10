5667d95f10c1fdd9e06d125ff87dd421a0225894
Aditya Sinha <adityasinha361@gmail.com>
2023-03-10 11:29:48 +0530

    feat: add collection route in index.js

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const port = 4000;
 const cors = require('cors');
 app.use(express.json());
 app.use(cors());
-const recordRoute = require('./src/routes/recordRoute');
-app.use('/record', recordRoute);
+const recordRoutes = require('./src/routes/recordRoute');
+app.use('/record', recordRoutes);
+
+const collectionRoutes = require('./src/routes/collectionRoute');
+app.use('/collection', collectionRoutes);
+
 app.listen(port, () => {
   console.log(`started listening on port ${port}`);
 });
