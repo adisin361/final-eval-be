d11d60741ce022a30d818a84bdff20a18b8f7e90
Aditya Sinha <adityasinha361@gmail.com>
2023-03-10 11:29:25 +0530

    feat: add routes for collection service

---
+++ src/routes/collectionRoute.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const collectionController = require('../controllers/collectionController');
+
+router.route('/save')
+  .post(collectionController.addRecord);
+
+router.route('/update')
+  .post(collectionController.editRecord);
+
+router.route('/field')
+  .delete(collectionController.deleteRecord);
+
+router.route('/details')
+  .get(collectionController.listCollections)
+  .post(collectionController.getDataOfCollection);
+
+module.exports = router;
